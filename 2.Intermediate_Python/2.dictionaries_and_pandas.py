143e5900314d97ecf26e927ccc13f96cc3d61047
Mary Kate Anecito <marykate09@protonmail.com>
2024-04-27 09:26:59 +0800

    Added new lesson about pandas

--- 2.Intermediate_Python/2.dictionaries_and_pandas.py
+++ 2.Intermediate_Python/2.dictionaries_and_pandas.py
@@ -1,21 +1,129 @@
+import pandas as pd
+
 # Dictionary vs List
-south_east_asia_countries_population = {
-    'Indonesia': 273.5,
-    'Thailand': 69.8,
-    'Malaysia': 32.4,
-    'Singapore': 5.7,
-    'Philippines': 110.8,
-    'Vietnam': 97.3,
-    'Myanmar': 54.4,
-    'Cambodia': 16.7,
-    'Laos': 7.3,
-    'Brunei': 0.4
+# Population in millions, area in square kilometers
+south_east_asia_countries_info = {
+    'Indonesia':
+        {
+            'Capital': 'Jakarta',
+            'Area': 1904569,
+            'Population': 273.5,
+        },
+    'Thailand':
+        {
+            'Capital': 'Bangkok',
+            'Area': 513120,
+            'Population': 69.8,
+        },
+    'Malaysia':
+        {
+            'Capital': 'Kuala Lumpur',
+            'Area': 330803,
+            'Population': 32.4,
+        },
+    'Singapore':
+        {
+            'Capital': 'Singapore',
+            'Area': 722.5,
+            'Population': 5.7
+        },
+    'Philippines':
+        {
+            'Capital': 'Manila',
+            'Area': 300000,
+            'Population': 110.8,
+        },
+    'Vietnam':
+        {
+            'Capital': 'Hanoi',
+            'Area': 331212,
+            'Population': 97.3
+        },
+    'Myanmar':
+        {
+            'Capital': 'Naypyidaw',
+            'Area': 676578,
+            'Population': 54.4
+        },
+    'Cambodia':
+        {
+            'Capital': 'Phnom Penh',
+            'Area': 181035,
+            'Population': 16.7
+        },
+    'Laos':
+        {
+            'Capital': 'Vietiane',
+            'Area': 236800,
+            'Population': 7.3
+        },
+    'Brunei':
+        {
+            'Capital': 'Bandar Seri Begawan',
+            'Area': 236800,
+            'Population': 0.4
+        },
+    'Norway':
+        {
+            'Capital': 'Oslo',
+            'Area': 385207,
+            'Population': 5.5
+        }
 }
 
-print(south_east_asia_countries_population['Philippines']) # Uses key
+# Accessing a value element using keys
+print(south_east_asia_countries_info['Philippines'])
+print(south_east_asia_countries_info['Philippines']['Population'])
 
-south_east_asia_countries = ['Indonesia', 'Thailand', 'Malaysia', 'Singapore', 'Philippines', 'Vietnam', 'Myanmar', 'Cambodia', 'Laos', 'Brunei'] # Can be converted to dictionary
+# Printing keys of a dictionary
+print(south_east_asia_countries_info.keys())
+
+# Deleting a key-value pair
+del(south_east_asia_countries_info['Norway'])
+
+# Changing the value of an existing key
+south_east_asia_countries_info['Philippines'] = 109.0
+
+south_east_asia_countries = ['Indonesia', 'Thailand', 'Malaysia', 'Singapore', 'Philippines', 'Vietnam', 'Myanmar',
+                             'Cambodia', 'Laos', 'Brunei'] # Can be converted to dictionary
 south_east_asia_pop = [273.5, 69.8, 32.4, 5.7, 110.8, 97.3, 54.4, 16.7, 7.3, 0.4]  # in millions
 
 ind_phil = south_east_asia_countries.index('Philippines')
-print(south_east_asia_pop[ind_phil]) # Uses index
+# Accessing a value element using index
+print(south_east_asia_pop[ind_phil])
+
+# Pandas
+# Dictionary can be converted as Pandas' DataFrame
+# When working with dataframes, remember that the keys correspond to column names and their associated values represent
+# the data in each row.
+south_east_asia_countries_dataFrame_v1 = pd.DataFrame(south_east_asia_countries_info)
+print(south_east_asia_countries_dataFrame_v1)
+print()
+
+
+# This is how the dictionary must be initialized and assigned to
+south_east_asia_countries_info_v2 = {
+    'Countries': ['Indonesia', 'Thailand', 'Malaysia', 'Singapore', 'Philippines', 'Vietnam', 'Myanmar', 'Cambodia', 'Laos', 'Brunei'],
+    'Capital': ['Jakarta', 'Bangkok', 'Kuala Lumpur', 'Singapore', 'Manila', 'Hanoi', 'Naypyidaw', 'Phnom Penh', 'Vietiane', 'Bandar Seri Begawan'],
+    'Area': [1904569, 513120, 330803, 722.5, 300000, 331212, 676578, 181035, 236800, 5765],
+    'Population': [273.5, 69.8, 32.4, 5.7, 110.8, 97.3, 54.4, 16.7, 7.3, 0.4]
+}
+
+south_east_asia_countries_dataFrame_v2 = pd.DataFrame(south_east_asia_countries_info_v2)
+print(south_east_asia_countries_dataFrame_v2)
+print()
+
+# Instead of using row index based, it can be changed to row label based
+south_east_asia_countries_dataFrame_v2.index = ['ID', 'TH', 'MY', 'SG', 'PH', 'VN', 'MM', 'KH', 'LA', 'BN']
+print(south_east_asia_countries_dataFrame_v2)
+print()
+
+# Importing csv file to DataFrame using .read_csv() method
+south_east_asia_countries_imported_from_csv_dataFrame_v3 = pd.read_csv('south_east_asia_countries_info.csv')
+print(south_east_asia_countries_imported_from_csv_dataFrame_v3)
+print()
+
+# index_col means defining the first (0th) column of the data as the index of the resulting DataFrame,
+# If index_col is not used, the default index is integer based
+south_east_asia_countries_imported_from_csv_dataFrame_v4 = pd.read_csv('south_east_asia_countries_info.csv', index_col=0)
+print(south_east_asia_countries_imported_from_csv_dataFrame_v4)
