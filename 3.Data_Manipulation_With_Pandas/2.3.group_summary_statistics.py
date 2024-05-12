e3b884d9ab429ae61bef659acf79dab5a773e7b4
Mary Kate Anecito <marykate09@protonmail.com>
2024-05-12 12:51:28 +0800

    Added new examples for group summary statistics

--- 3.Data_Manipulation_With_Pandas/2.3.group_summary_statistics.py
+++ 3.Data_Manipulation_With_Pandas/2.3.group_summary_statistics.py
@@ -10,7 +10,27 @@ print("Data Scientist ", employees[employees["position"] == "Data Scientist"]["s
 print("Product Manager ", employees[employees["position"] == "Project Manager"]["salary"].mean())
 print("Software Engineer ", employees[employees["position"] == "Software Engineer"]["salary"].mean())
 
-
 # Version 2 - Improved version
 print("Average Salary of Each Position in a Tech Industry")
 print(employees.groupby("position")["salary"].mean())
+
+print("Minimum, Maximum, Total, Average, and Median Salary of Each Position in Tech Industry")
+print(employees.groupby("position")["salary"].agg(["min", "max", "sum", "mean", "median"]))
+
+dogFile = pd.read_csv('dogs.csv')
+dogs = pd.DataFrame(dogFile)
+
+print("Average Weight of Each Dog Breeds")
+print(dogs.groupby("breed")["weight_kg"].mean())
+
+print("Minimum, Maximum, Total, Average, and Median Weight of Each Breed per Color")
+print(dogs.groupby(["breed", "color"])["weight_kg"].agg(["min", "max", "sum", "mean", "median"]))
+
+# DataCamp examples
+data_analysts_salary = employees[employees["position"] == "Data Analyst"]["salary"].sum()
+data_scientists_salary = employees[employees["position"] == "Data Scientist"]["salary"].sum()
+product_managers_salary = employees[employees["position"] == "Project Manager"]["salary"].sum()
+software_engineers_salary = employees[employees["position"] == "Software Engineer"]["salary"].sum()
+total_salary = employees["salary"].sum()
+proportion_of_salary = [data_analysts_salary, data_scientists_salary, product_managers_salary, software_engineers_salary] / total_salary
+print(proportion_of_salary)
