2884b2d1a574bfc2f04ee159ffb70bddf7654817
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-05-23 23:32:44 +0800

    Added new lesson about inner joins.

---
+++ 4.Joining_Data_With_Pandas/1.inner_join.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+customersFile = pd.read_csv('customers.csv')
+ordersFile = pd.read_csv('orders.csv')
+
+customers = pd.DataFrame(customersFile)
+orders = pd.DataFrame(ordersFile)
+
+
+print(customers.shape)
+print(customers.columns)
+print(orders.shape)
+print(orders.columns)
+
+customers_orders = customers.merge(orders, on="customer_id")
+# adding suffixes parameter (left_indicator, right_indicator) if there are column names that has the same  name
+
+print(customers_orders)
