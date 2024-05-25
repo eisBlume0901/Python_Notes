5ed0800b5afbb9379ecdf6eb09e80a800de6d002
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-05-25 23:15:27 +0800

    Added new lesson about merging multiple dataframes for three tables

--- 4.Joining_Data_With_Pandas/1.3.merging_multiple_datarames.py
+++ 4.Joining_Data_With_Pandas/1.3.merging_multiple_datarames.py
@@ -22,3 +22,8 @@ print("Total Number of Chuck Taylor Shoes ordered")
 # Reminder: filterCritera is row, quantity is column (goal: to find a specific cell)
 print(shoemakers_orders_customers.loc[filterCriteria, 'quantity'].sum())
 
+# Reminder: groupby allows you to categorize data through a specific column variable and get their descriptive statistics.
+print("Total Number of Orders for Each Shoes")
+print(shoemakers_orders_customers.groupby(shoemakers_orders_customers["product"], as_index=False).agg({"quantity": "sum"}))
+# grouped = df.groupby(['column1', 'column2'], as_index=False).agg({'column3':'sum'})
+# as_index means the group labels are not set as index, they remain as columns and their default integer index is used
