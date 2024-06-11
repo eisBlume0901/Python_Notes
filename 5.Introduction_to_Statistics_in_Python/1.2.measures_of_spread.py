5002e2140552bdc279e8ed841e03c3858a2fbfaf
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-06-11 20:24:33 +0800

    Added new lesson about mean absolute deviation

--- 5.Introduction_to_Statistics_in_Python/1.2.measures_of_spread.py
+++ 5.Introduction_to_Statistics_in_Python/1.2.measures_of_spread.py
@@ -22,9 +22,28 @@ print(food_consumption["co2_emission"].quantile(np.linspace(0, 1, 6)))
 print(np.quantile(food_consumption["co2_emission"], [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]))
 print(food_consumption["co2_emission"].quantile(np.linspace(0, 1, 11)))
 
+# Histogram
 print(food_consumption.groupby("food_category")["co2_emission"].agg(["var", "std"]))
 food_consumption[food_consumption["food_category"] == "beef"].plot(kind="hist", x="co2_emission")
-plt.show()
+plt.show()  # Positively/right skewed - recommended to use median
 
 food_consumption[food_consumption["food_category"] == "pork"].plot(kind="hist", x="co2_emission")
-plt.show()
+plt.show()  # Positively/right skewed - recommended to use median
+
+# Mean Absolute Deviation (if you want to get the Median Absolute Deviation, you have to just use np.median)
+rice_consumption_co2_emission = food_consumption[food_consumption["food_category"] == "rice"]["co2_emission"]
+print(np.mean(np.abs(rice_consumption_co2_emission - np.mean(rice_consumption_co2_emission))))
+
+food_categories = np.unique(food_consumption["food_category"])
+for category in food_categories:
+    category_data = food_consumption[food_consumption["food_category"] == category]["co2_emission"]
+    mad = np.mean(np.abs(category_data - np.mean(category_data)))
+    print(f"Mean Absolute Deviation for {category}: {mad}")
+# Notice that the values of mean absolute deviation is lower than standard deviation
+
+# IQR
+emission_by_country = food_consumption.groupby("country")["co2_emission"].sum()
+q1 = np.quantile(emission_by_country, 0.25)
+q3 = np.quantile(emission_by_country, 0.75)
+iqr = q3 - q1
+print(f"Interquartile Range {iqr}")
