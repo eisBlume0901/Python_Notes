2e2c8ddd00a3f0dc8a7d88298844f31e996617cb
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-07-03 19:14:58 +0800

    Added new lesson about time-series data

---
+++ 6.Introduction_to_Data_Visualization_with_Matplotlib/2.1.plotting_time-series_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Time-series data (x = time), either parse the date or set pd.to_datetime()
+# breaks in line graph = NaN values
+
+# Read the CSV file
+co2_data = pd.read_csv("co2-long-term-concentration.csv")
+co2_df = co2_data.set_index("Year")
+print(co2_df.head())
+
+co2_df_from_1970_to_2023 = co2_df.loc[1970:2023]
+
+# Good method is to parse it to string and add 01-01 for each year to properly label it as year level data
+co2_df_from_1970_to_2023.index = co2_df_from_1970_to_2023.index.astype(str) + '-01-01'
+co2_df_from_1970_to_2023.index = pd.to_datetime(co2_df_from_1970_to_2023.index)
+print(co2_df_from_1970_to_2023.head())
+print(co2_df_from_1970_to_2023.tail())
+
+temperature_data = pd.read_csv("temperature-anomaly.csv")
+global_temperature = temperature_data[temperature_data["Entity"] == "Global"]
+global_temperature_df = pd.DataFrame(global_temperature)
+global_temperature_df_v2 = global_temperature_df.set_index("Year")
+print(global_temperature_df_v2.tail())
+global_temperature_df_v2.index = global_temperature_df_v2.index.astype(str) + "-01-01"
+print(global_temperature_df_v2.head())
+print(global_temperature_df_v2.tail())
+
+
+figure, axis = plt.subplots(2, 1)
+axis[0].plot(co2_df_from_1970_to_2023.index, co2_df_from_1970_to_2023["Long-run CO₂ concentration"])
+axis[0].set_xlabel("Year")
+axis[0].set_ylabel("CO₂ concentration (PPMV)")
+
+axis[1].plot(global_temperature_df_v2.index, global_temperature_df_v2["Global average temperature anomaly relative to 1961-1990"])
+axis[1].set_xlabel("Year")
+axis[1].set_ylabel("Global average temperature")
+
+plt.subplots_adjust(wspace=0.3, hspace=0.3)
+
+plt.show()
