f39dd58b2a922ba3e684cf5739632dc5f1667c98
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-12 18:42:15 +0800

    Added new lesson about bar charts

--- 6.Introduction_to_Data_Visualization_with_Matplotlib/3.1.bar_charts.py
+++ 6.Introduction_to_Data_Visualization_with_Matplotlib/3.1.bar_charts.py
@@ -1,18 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# world_happiness_2023 = pd.read_csv('WHR2023.csv', index_col=0)
-# # Using bar chart might not be appropriate for this data (scatter plot is much fitting to get the relationship between two or multiple variables)
-# # The data below is just use how to plot stacked bar charts
-# logged_gdp_per_capita_greater_than_10_80 = world_happiness_2023[world_happiness_2023["Logged GDP per capita"] > 10.80]
-# figure, axis = plt.subplots(figsize=(20, 15))
-# axis.bar(logged_gdp_per_capita_greater_than_10_80.index, logged_gdp_per_capita_greater_than_10_80["Logged GDP per capita"])
-# axis.bar(logged_gdp_per_capita_greater_than_10_80.index, logged_gdp_per_capita_greater_than_10_80["Healthy life expectancy"], bottom=logged_gdp_per_capita_greater_than_10_80["Logged GDP per capita"])
-# axis.set_xticklabels(logged_gdp_per_capita_greater_than_10_80.index, rotation=90)
-# axis.set_ylabel("Logged GDP per capita and its Healthy life expectancy")
-# axis.set_xlabel("Countries")
-# axis.set_title("Countries that has a logged GDP per capita greater than 10.80 along with its healthy life expectancy")
-# plt.tight_layout()
-# plt.show()
+sales_icecream = [
+    {
+        "Village": "Mushroom Village",
+        "Strawberry": 25,
+        "Vanilla": 10,
+        "Pistachio": 5,
+        "Mint Chocolate Chip": 15
+    },
+    {
+        "Village": "Marine Village",
+        "Strawberry": 10,
+        "Vanilla": 5,
+        "Pistachio": 30,
+        "Mint Chocolate Chip": 20
+    },
+    {
+        "Village": "Sky Village",
+        "Strawberry": 17,
+        "Vanilla": 12,
+        "Pistachio": 15,
+        "Mint Chocolate Chip": 25
+    }
+]
 
-# The data appropriate for stack bar charts are categorical data
+sales_icecream_df = pd.DataFrame(sales_icecream)
+sales_icecream_df = sales_icecream_df.set_index("Village")
+print(sales_icecream_df.head())
+
+figure, axis = plt.subplots()
+
+axis.bar(sales_icecream_df.index, sales_icecream_df["Strawberry"], label="Strawberry", color="pink")
+axis.bar(sales_icecream_df.index, sales_icecream_df["Vanilla"], bottom=sales_icecream_df["Strawberry"], label="Vanilla", color="yellow")
+axis.bar(sales_icecream_df.index, sales_icecream_df["Mint Chocolate Chip"], bottom=sales_icecream_df["Vanilla"] + sales_icecream_df["Strawberry"], label="Mint Chocolate Chip", color="green")
+# Set the positions of the x-ticks
+axis.set_xticks(range(len(sales_icecream_df.index)))
+
+# Set the labels for the x-ticks with rotation for better readability
+axis.set_xticklabels(sales_icecream_df.index, rotation=90)
+axis.legend()
+plt.tight_layout()
+plt.show()
