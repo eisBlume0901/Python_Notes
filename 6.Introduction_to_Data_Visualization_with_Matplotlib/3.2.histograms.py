6ec11818a50dfdccd5fa6a0592cc5802aac6e5aa
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-07-16 18:36:50 +0800

    Added new lesson about plotting histogram with matplotlib.pyplot

--- 6.Introduction_to_Data_Visualization_with_Matplotlib/3.2.histograms.py
+++ 6.Introduction_to_Data_Visualization_with_Matplotlib/3.2.histograms.py
@@ -1,3 +1,16 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
+np.random.seed(1500)
+height_in_germany_women = np.random.normal(165, 6, 100)
+height_in_philippines_women = np.random.normal(150, 5, 100)
+
+# Creating overlapping histograms
+figure, axis = plt.subplots()
+axis.hist(height_in_germany_women, label="Germany", histtype="step", bins=5, color="red")
+axis.hist(height_in_philippines_women, label="Philippines", histtype="step", bins=5, color="blue")
+axis.legend()
+axis.set_xlabel("Height (cm)")
+axis.set_ylabel("Number of observations")
+axis.set_title("Height of Women in Germany and Philippines")
+plt.show()
