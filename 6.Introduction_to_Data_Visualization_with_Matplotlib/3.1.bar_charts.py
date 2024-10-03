01938de5d269544d921b63dd4f78a1040dff7271
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-10-03 19:31:38 +0800

    Added some bar graphs related to scores

--- 6.Introduction_to_Data_Visualization_with_Matplotlib/3.1.bar_charts.py
+++ 6.Introduction_to_Data_Visualization_with_Matplotlib/3.1.bar_charts.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 sales_icecream = [
     {
         "Village": "Mushroom Village",
@@ -46,3 +47,36 @@ axis.set_title("Ice Cream Sales in Villages")
 axis.legend()
 plt.tight_layout()
 plt.show()
+
+data = {
+    'date_time': ['2025-01-03 10:45:21', '2025-01-03 11:31:22', '2025-01-04 09:12:15', '2025-01-04 12:17:03', '2025-01-05 08:45:21', '2025-01-05 10:45:21', '2025-01-06 09:45:21'],
+    'score': [21, 35, 41, 29, 28, 31, 32],
+    'overall_score': [50, 50, 50, 30, 35, 40, 40]
+}
+
+data_df = pd.DataFrame(data)
+
+data_df['percentage'] = (data_df['score'] / data_df['overall_score']) * 100
+
+def set_color(percentage_series):
+    colors = []
+    for percentage in percentage_series:
+        if percentage < 70:
+            colors.append('#FEE140')
+        else:
+            colors.append('#DA384C')
+    return colors
+
+colors = set_color(data_df['percentage'])
+
+figure1, axis1 = plt.subplots(figsize=(12, 7))
+axis1.bar(data_df['date_time'], data_df['percentage'], color=colors)
+axis1.set_xticks(range(len(data_df['date_time'])))
+axis1.set_xticklabels(data_df['date_time'], rotation=35)
+axis1.set_yticks(range(0, 101, 10))
+axis1.set_ylabel("Score Percentage (%)")
+axis1.set_xlabel("Date and Time")
+plt.tight_layout()
+plt.show()
+
+
